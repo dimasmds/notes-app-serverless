792d730b0bbedd94a1c35589f80750de5ab40254
Dimas Saputra <dimas.maulanads@gmail.com>
2021-11-27 20:42:30 +0700

    Add decode function

--- src/Infrastructures/security/JwtTokenizeImpl.ts
+++ src/Infrastructures/security/JwtTokenizeImpl.ts
@@ -1,9 +1,13 @@
-import { sign } from 'jsonwebtoken';
+import { sign, decode } from 'jsonwebtoken';
 import JwtTokenize, { TokenPayload } from '../../Applications/security/JwtTokenize';
 
 class JwtTokenizeImpl implements JwtTokenize {
   async createToken(payload: TokenPayload, secret: string, expiresIn: number): Promise<string> {
     return sign(payload, secret, { expiresIn: 60 * expiresIn });
+  }
+
+  async decode(token: string): Promise<TokenPayload> {
+    return decode(token) as TokenPayload;
   }
 }
 
--- src/Infrastructures/security/_test/JwtTokenizeImpl.test.ts
+++ src/Infrastructures/security/_test/JwtTokenizeImpl.test.ts
@@ -1,5 +1,6 @@
-import { JwtPayload, verify } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
 import JwtTokenizeImpl from '../JwtTokenizeImpl';
+import { TokenPayload } from '../../../Applications/security/JwtTokenize';
 
 describe('JwtTokenizeImpl', () => {
   const jwtTokenizeImpl = new JwtTokenizeImpl();
@@ -19,10 +20,23 @@ describe('JwtTokenizeImpl', () => {
       expect(typeof token).toEqual('string');
 
       // Verify
-      const decoded = verify(token, 'ssh') as JwtPayload;
+      const decoded = verify(token, 'ssh') as TokenPayload;
       expect(decoded.userId).toEqual(payload.userId);
       expect(decoded.exp).toBeDefined();
       expect(decoded.iat).toBeDefined();
+    });
+  });
+
+  describe('decode', () => {
+    it('should decode token correctly', async () => {
+      // Arrange
+      const token = sign({ foo: 'bar' }, 'secret');
+
+      // Action
+      const result = await jwtTokenizeImpl.decode(token) as any;
+
+      // Assert
+      expect(result.foo).toEqual('bar');
     });
   });
 });
