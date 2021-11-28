683091f6fe261478d9928dfd27e4cb390d17eae4
Dimas Saputra <dimas.maulanads@gmail.com>
2021-11-28 17:13:59 +0700

    add domain error message

--- src/Commons/execptions/DomainToHTTPErrorTranslator.ts
+++ src/Commons/execptions/DomainToHTTPErrorTranslator.ts
@@ -1,12 +1,15 @@
 import InvariantError from './InvariantError';
 import NotFoundError from './NotFoundError';
 import AuthenticationError from './AuthenticationError';
+import AuthorizationError from './AuthorizationError';
 
 class DomainToHttpErrorTranslator {
   static Directories: any = {
     'USER_CREATION.USERNAME_ALREADY_TAKEN': new InvariantError('username already taken'),
     'USER_LOGIN.USERNAME_NOT_FOUND': new NotFoundError('username is not found'),
     'USER_LOGIN.PASSWORD_IS_NOT_VALID': new AuthenticationError('wrong password'),
+    'GET_NOTE_USE_CASE.NOTE_NOT_FOUND': new NotFoundError('note is not found'),
+    'GET_NOTE_USE_CASE.USER_NOT_OWNED_THE_NOTE': new AuthorizationError('you are not allowed to access this note'),
   }
 
   static translate(domainError: Error): Error {
